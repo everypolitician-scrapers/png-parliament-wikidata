f4d90c788078877dc4e0284eb125f9099b916696
Tony Bowden <tony@tmtm.com>
2018-03-06 08:50:10 +0000

    Update enwiki Category

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 require 'wikidata/fetcher'
 
 fr = WikiData::Category.new( 'Catégorie:Personnalité politique papouasienne', 'fr').member_titles
-en = WikiData::Category.new( 'Category:Papua New Guinean politicians', 'en').member_titles
+en = WikiData::Category.new( 'Category:Members of the National Parliament of Papua New Guinea', 'en').member_titles
 de = WikiData::Category.new( 'Kategorie:Politiker (Papua-Neuguinea)', 'de').member_titles
 el = WikiData::Category.new( 'Κατηγορία:Πολιτικοί της Παπούα Νέας Γουινέας', 'el').member_titles
 es = WikiData::Category.new( 'Categoría:Políticos de Papúa Nueva Guinea', 'es').member_titles
